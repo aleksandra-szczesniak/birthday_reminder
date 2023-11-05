c1f3e87cdf220b2a7910ebf1c56e34afa56a0027
Aleksandra Szcześniak <136647548+aleksandra-szczesniak@users.noreply.github.com>
2023-11-05 15:02:50 +0100

    birthday_reminder.py

--- birthday_reminder.py
+++ birthday_reminder.py
@@ -1,8 +1,8 @@
 from datetime import date
 
 users = [
-    {"name": "Marta", "birthday": date(1994, 11, 1)},
-    {"name": "Magdalena", "birthday": date(1994, 11, 3)},
+    {"name": "Marta", "birthday": date(1994, 11, 11)},
+    {"name": "Magdalena", "birthday": date(1994, 11, 12)},
     {"name": "Milena", "birthday": date(1994, 11, 4)},
     {"name": "Paulina", "birthday": date(1994, 11, 5)},
     {"name": "Justyna", "birthday": date(1995, 11, 6)},
@@ -12,7 +12,7 @@ users = [
 
 def get_birthday_per_week(users):
     today = date.today()
-    birthday_dict = {i: [] for i in range(7)}
+    birthday_dict = {i: [] for i in range(5)}
 
     for user in users:
         days_until_birthday = (user["birthday"].replace(
@@ -20,11 +20,13 @@ def get_birthday_per_week(users):
         if 0 <= days_until_birthday < 7:
             day_of_week = (today.weekday() + days_until_birthday) % 7
             if day_of_week >= 5:
+                days_until_birthday += (7 - day_of_week)
                 day_of_week = 0
-            birthday_dict[day_of_week].append(user["name"])
 
-    days_of_week = ["Monday", "Tuesday", "Wednesday",
-                    "Thursday", "Friday", "Saturday", "Sunday"]
+            if days_until_birthday < 7:
+                birthday_dict[day_of_week].append(user["name"])
+
+    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
     for day, names in birthday_dict.items():
         if names:
